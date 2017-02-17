5cd884597cc0e94d0f49b5d8a5c3cd8b66fbcb82
Hannah Hall <hannahkhall13@gmail.com>
2017-02-17 11:18:42 -0600

    import redirect

--- tutorial/tutorial_app/views.py
+++ tutorial/tutorial_app/views.py
@@ -3,7 +3,7 @@ from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.contrib.auth import logout, login, authenticate
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
