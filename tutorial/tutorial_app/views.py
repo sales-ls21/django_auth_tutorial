5b5d999b85fe705e3835c04ba2ec4d770995a995
LaDonna <boxgirl85@gmail.com>
2017-02-20 10:02:50 -0600

    reference for login/logout

--- tutorial/tutorial_app/views.py
+++ tutorial/tutorial_app/views.py
@@ -20,12 +20,24 @@ class Register(TemplateView):
 
 
 def register_user(request):
-    pass
+    data = request.POST
+    User.objects.create_user(username=data['username'], email=data['email'],
+    	first_name=data['first_name'], password=data['password'], last_name=data['last_name'])
+    return login_user(request)
 
 
 def login_user(request):
-    pass
+    data = request.POST
+    username= data['username']
+    password = data['password']
+    user= authenticate(username=username, password=password)
 
+    if user is not None:
+    	login(request=request, user=user)
+    else:
+    	return HttpResponseRedirect(redirect_to='/')
+    return HttpResponseRedirect(redirect_to='/success')
 
 def logout_user(request):
-    pass
+	logout(request)
+	return HttpResponseRedirect(redirect_to='/')    
